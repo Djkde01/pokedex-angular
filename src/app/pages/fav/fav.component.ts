efde60e482cd942af57ef8e642e139669deb171f
Djkde01 <sergio.estrella@utp.edu.co>
2022-10-04 01:35:22 -0500

    feat (Fav page)

--- src/app/pages/fav/fav.component.ts
+++ src/app/pages/fav/fav.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/model/pokemon';
 
 @Component({
   selector: 'app-fav',
   templateUrl: './fav.component.html',
-  styleUrls: ['./fav.component.css']
+  styleUrls: ['./fav.component.css'],
 })
 export class FavComponent implements OnInit {
+  favList: Array<Pokemon> = JSON.parse(localStorage.getItem('favList') || '{}');
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
